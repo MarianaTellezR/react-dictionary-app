f9ed1a2c3497f06cc6b0b1ca580451069651ffe7
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-08-14 14:25:52 -0600

    Created search input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo_mtr.png";
 import background from "./background-dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -23,7 +24,31 @@ function App() {
         className="Dictionary-body"
         style={{ backgroundImage: `url(${background})` }}
       >
-        Hello
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <p>
+            Designed and coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/mariana-tellez-resendiz/"
+              target="_blank"
+              rel="noreferrer"
+              title="LinkedIn"
+            >
+              Mariana Tellez Resendiz
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/MarianaTellezR/react-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+              title="GitHub Repository"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
