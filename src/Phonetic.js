a227b1ef219c454a264ef11789d058c9899a0b53
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-08-24 14:06:32 -0600

    Updated Phonetics styling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,13 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <br />
+        <i class="fa-solid fa-volume-high"></i>
+      </a>   {"  "}
       {props.phonetic.text}
     </div>
   );
