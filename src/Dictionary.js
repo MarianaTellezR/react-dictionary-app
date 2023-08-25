642f944ab72149114d410ac550c97f587267bbda
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-08-25 12:59:26 -0600

    Included Pexels API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,21 +8,33 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
-    // console.log(response.data);
-    // console.log(response.data.meanings[0].definition);
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
   }
 
   function search() {
     // Used API Documentation: https://www.shecodes.io/learn/apis/dictionary
     // API Used on the SheCodes video: https://dictionaryapi.dev/
 
-    // let apiKey = "8f6148bf7t4049b3979ae80dba7bo608";
-    // let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "WoagrkpifpnozEiw2OMJ6YwWPwEg2lUgiLysn49r2dpjy5W4wwseXuM2";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+
+    let headers = { "Authorization": `Bearer ${pexelsApiKey}` };
+    
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
